2fde454832192e6a0210b4c033233e8162fb6ed6
max-kresh <max.kreshch@gmail.com>
2025-01-28 12:35:44 +0200

    IT22-17 Migration fix

--- contacts/migrations/0006_contact_category.py
+++ contacts/migrations/0006_contact_category.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0005_alter_contact_address'),
+        ('contacts', '0006_alter_contact_mobile_number_and_more'),
     ]
 
     operations = [
