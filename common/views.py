6a340bd56e17586b08e7bb1631a753a660519a8e
Muhammed Hasan Karadirek <muhammed.karadirek@motopp.nl>
2025-03-19 11:34:54 +0100

    ApiHomeView updated for clean code

--- common/views.py
+++ common/views.py
@@ -356,72 +356,37 @@ class ApiHomeView(APIView):
         pagination_request = self.PaginationRequest()
 
         accounts= Account.objects.filter(org=request.profile.org)
+        contacts = Contact.objects.filter(org=request.profile.org)
+        leads = Lead.objects.filter(org=request.profile.org)
+        opportunities = Opportunity.objects.filter(org=request.profile.org)
+        
+        if self.request.profile.role != Constants.ADMIN and not self.request.user.is_superuser:
+            accounts = accounts.filter(
+                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
+            )
+            contacts = contacts.filter(
+                Q(assigned_to__id__in=self.request.profile)
+                | Q(created_by=self.request.profile.user)
+            )
+            leads = leads.filter(
+                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
+            )
+            opportunities = opportunities.filter(
+                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
+            )
         open_accounts = accounts.filter(status="open", org=request.profile.org)
         closed_accounts = accounts.filter(status="close", org=request.profile.org)
 
-        contacts = Contact.objects.filter(org=request.profile.org)
-
-        leads = Lead.objects.filter(org=request.profile.org)
         converted_leads =leads.filter(status="converted")
         closed_leads = leads.filter(status="closed")
         assigned_leads = leads.filter(status="assigned")
         in_process_leads = leads.filter(status="in process")
         recycled_leads = leads.filter(status="recycled")
 
-        opportunities = Opportunity.objects.filter(org=request.profile.org)
         in_process_opportunities = opportunities.exclude(Q(stage="CLOSED WON") | Q(stage="CLOSED LOST"))
         won_opportunities = opportunities.filter(stage="CLOSED WON")
         lost_opportunities = opportunities.filter(stage="CLOSED LOST")
 
-
-
-        if self.request.profile.role != Constants.ADMIN and not self.request.user.is_superuser:
-            accounts = accounts.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            open_accounts = open_accounts.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            closed_accounts = closed_accounts.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-
-            contacts = contacts.filter(
-                Q(assigned_to__id__in=self.request.profile)
-                | Q(created_by=self.request.profile.user)
-            )
-
-            leads = leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            converted_leads = converted_leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            closed_leads = closed_leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            assigned_leads = assigned_leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            in_process_leads = in_process_leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            recycled_leads = recycled_leads.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-
-            opportunities = opportunities.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            in_process_opportunities = in_process_opportunities.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            won_opportunities = won_opportunities.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
-            lost_opportunities = lost_opportunities.filter(
-                Q(assigned_to=self.request.profile) | Q(created_by=self.request.profile.user)
-            )
         accounts_paginated = self.paginator.paginate_queryset(accounts.order_by("-created_at"), pagination_request, view=self)
         leads_paginated = self.paginator.paginate_queryset(leads.order_by("-created_at"), pagination_request, view=self)
         contacts_paginated = self.paginator.paginate_queryset(contacts.order_by("-created_at"), pagination_request, view=self)
@@ -429,7 +394,7 @@ class ApiHomeView(APIView):
         context = {}
 
         context["accounts"] = {"open_accounts": open_accounts.count(), "closed_accounts": closed_accounts.count(), "last_five_accounts": AccountSerializer(accounts_paginated, many=True).data}
-        context["contacts"] = {"contacts": contacts.count(), "last_five_contacts": ContactSerializer(contacts, many=True).data}
+        context["contacts"] = {"contacts": contacts.count(), "last_five_contacts": ContactSerializer(contacts_paginated, many=True).data}
         context["leads"] = {"converted_leads": converted_leads.count(), "closed_leads": closed_leads.count(), "assigned_leads": assigned_leads.count(), "in_process_leads": in_process_leads.count(), "recycled_leads": recycled_leads.count(), "last_five_leads": LeadSerializer(leads_paginated, many=True).data}
         context["opportunities"] = {"in_process_opportunities": in_process_opportunities.count(), "won_opportunities": won_opportunities.count(), "lost_opportunities": lost_opportunities.count(), "last_five_opportunities": OpportunitySerializer(opportunities_paginated, many=True).data}
         return Response(context, status=status.HTTP_200_OK)
